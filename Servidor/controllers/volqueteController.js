e1474bb83d6c36f15cabda4e95e36f22330cb46e
gzammataro <gustavozammataro@gmail.com>
2023-07-01 03:49:24 -0300

    se agrega método eliminarSolicitudVolquete en el servidor

--- Servidor/controllers/volqueteController.js
+++ Servidor/controllers/volqueteController.js
@@ -2,12 +2,26 @@ const Volquete = require("../models/Volquete")
 
 exports.crearSolicitudVolquete = async(req,res) => {
     try {
-       let volquete;
-       volquete = new Volquete(req.body);
-       await volquete.save();
-       res.status(200).send(volquete);
+       let volquetes;
+       volquetes = new Volquete(req.body);
+       await volquetes.save();
+       res.status(200).send(volquetes);
     } catch (error) {
        console.log(error)
        res.status(500).send("Hubo un error al crear la solicitud del volquete")
     }
  }
+
+ exports.eliminarSolicitudVolquete = async(req, res) => {
+   try {
+       let solicitudesVolquetes = await Volquete.findById(req.params.id);
+       if(!solicitudesVolquetes){
+           res.status(404).json("no existe la solicitud a eliminar");
+       }
+       await Volquete.findOneAndRemove({_id:req.params.id})
+       res.status(200).json("La solicitud se eliminó con éxito");
+   } catch (error) {
+       console.log(error);
+       res.status(500).send("Hubo un error al intentar elimiar la solicitud - E0005");
+   }
+}
--- Servidor/routes/routerVolquete.js
+++ Servidor/routes/routerVolquete.js
@@ -3,6 +3,6 @@ const routerVolquete = express.Router();
 const volqueteController = require('../controllers/volqueteController')
 
 routerVolquete.post('/', volqueteController.crearSolicitudVolquete);
-
+routerVolquete.delete('/:id', volqueteController.eliminarSolicitudVolquete);
 
 module.exports = routerVolquete;
